17a69568991ef93001b34ecbeb79e4afce4444cb
LiQifeng <1517705600@qq.com>
2023-08-01 19:45:36 +0800

    add blog site

--- config/site.config.js
+++ config/site.config.js
@@ -53,13 +53,17 @@ module.exports = {
   // [OPTIONAL] This is an array of names and links for setting your social information and links.
   // In the latest update, all brand icons inside font awesome is supported and the icon to render is based on the name
   // you provide. See the documentation for details.
-  // links: [
-  //   {
-  //     name: 'GitHub',
-  //     link: 'https://github.com/spencerwooo/onedrive-vercel-index',
-  //   },
-  // ],
-  links: [],
+  links: [
+    {
+      name: 'Blog',
+      link: 'https://belikov.top'
+    },
+    {
+      name: 'airbnb',
+      link: 'https://gpt.belikov.top'
+    }
+  ],
+  // links: [],
 
 
   // This is a day.js-style datetime format string to format datetimes in the app. Ref to
